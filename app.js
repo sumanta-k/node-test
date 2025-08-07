d34afb05204f38a3e9544651baff70f87947ad98
sumanta kumar mohanta <mohantasumanta601@gmail.com>
2025-08-07 10:40:45 +0530

    Serve "Hello World" by doing res.write() and res.end()

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+    res.write("<!DOCTYPE>");
+    res.write("<html>");
+    res.write("<head><meta charset='utf-8'>");
+    res.write("<body><h1>Hello World!</h1></body>");
+    res.write("</html>");
+    res.end();
+
     console.log(`Your server is running on port 3000`);
     console.log(`Let's see what are the stuff "req" does have!`);
     console.log(req);
