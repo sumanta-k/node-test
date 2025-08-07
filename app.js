9e034f107bafc9f4f99085bd6d4e6d9f5d747d42
sumanta kumar mohanta <mohantasumanta601@gmail.com>
2025-08-07 09:55:42 +0530

    Add request's internal stuff To get more thoroughly

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     console.log(`Your server is running on port 3000`);
+    console.log(`Let's see what are the stuff "req" does have!`);
+    console.log(req);
 });
 
 server.listen(3000);
